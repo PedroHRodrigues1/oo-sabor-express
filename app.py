ebaec31fc1bed051b3d6d4704132a1692875dff4
PedroHRodrigues1 <122585029+PedroHRodrigues1@users.noreply.github.com>
2024-01-11 22:50:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from models.restaurante import Restaurante
-
+from models.cardapio.prato import Prato
+from models.cardapio.bebida import Bebida
 
 churrascaria = Restaurante('Churras', 'Churrascaria')
-churrascaria.receber_avaliacao('Pedro', 10)
-churrascaria.alternar_estado()
+bebida_coca = Bebida('Coca Cola', 10.00, '2L')
+bebida_coca.aplicar_desconto()
+prato_feijoada = Prato('Feijoada', 50.00, 'A melhor feijoada da Cidade')
+prato_feijoada.aplicar_desconto()
+
+churrascaria.adicionar_cardapio(bebida_coca)
+churrascaria.adicionar_cardapio(prato_feijoada)
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    churrascaria.exibir_cardapio
 
 
 if __name__ == '__main__':
